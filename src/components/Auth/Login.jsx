7a5d0bd764e857953f499363a3ba17586da13f59
Mher Hovhannisyan <mh3r99@gmail.com>
2022-12-18 18:30:58 +0400

    sign in user

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,7 +1,107 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import {
+  Grid,
+  Form,
+  Segment,
+  Button,
+  Header,
+  Message,
+  Icon,
+} from "semantic-ui-react";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { db } from "../../firebase";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const { email, password } = formData;
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (isFormValid()) {
+      setLoading(true);
+
+      try {
+        const auth = getAuth();
+
+        const userCredential = await signInWithEmailAndPassword(
+          auth,
+          email,
+          password
+        );
+
+        const user = userCredential.user;
+      } catch (error) {
+        setError(error.message);
+      }
+
+      setError(null);
+      setLoading(false);
+    }
+  };
+
+  const isFormValid = () => email && password;
+
+  const handelInputError = (error, inputName) =>
+    error?.toLowerCase().includes(inputName) ? "error" : "";
+
+  return (
+    <Grid textAlign="center" verticalAlign="middle" className="app">
+      <Grid.Column style={{ maxWidth: 450 }}>
+        <Header as="h1" icon color="violet" textAlign="center">
+          <Icon name="code branch" color="violet" />
+          Login to DevChat
+        </Header>
+        <Form size="large" onSubmit={handleSubmit}>
+          <Segment stacked>
+            <Form.Input
+              fluid
+              name="email"
+              icon="mail"
+              iconPosition="left"
+              placeholder="Email Address"
+              type="email"
+              onChange={handleChange}
+              value={email || ""}
+              className={handelInputError(error, "email")}
+            />
+
+            <Form.Input
+              fluid
+              name="password"
+              icon="lock"
+              iconPosition="left"
+              placeholder="Password"
+              type="password"
+              onChange={handleChange}
+              value={password || ""}
+              className={handelInputError(error, "password")}
+            />
+
+            <Button
+              disabled={loading}
+              className={loading ? "loading" : ""}
+              color="violet"
+              fluid
+              size="large"
+            >
+              Submit
+            </Button>
+          </Segment>
+        </Form>
+        {error && <Message error>{error}</Message>}
+        <Message>
+          Don't have an account? <Link to="/register">Register</Link>
+        </Message>
+      </Grid.Column>
+    </Grid>
+  );
 };
 
 export default Login;
