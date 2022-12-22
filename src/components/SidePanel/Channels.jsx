cff26bbd5d1ba6334343f4e22620d1545370ec4b
Mher Hovhannisyan <mh3r99@gmail.com>
2022-12-22 23:11:36 +0400

    Create Modal for Adding Channels

--- src/components/SidePanel/Channels.jsx
+++ src/components/SidePanel/Channels.jsx
@@ -1,22 +1,68 @@
 import React, { useState } from "react";
-import { Icon, Menu } from "semantic-ui-react";
+import { Button, Form, Icon, Input, Menu, Modal } from "semantic-ui-react";
 
 const Channels = () => {
   const [channels, setChannels] = useState([]);
+  const [modal, setModal] = useState(false);
+  const [formData, setFormData] = useState({
+    channelName: "",
+    channelDetails: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
 
   return (
-    <Menu.Menu
-      style={{
-        paddingBottom: "2em",
-      }}
-    >
-      <Menu.Item>
-        <span>
-          <Icon name="exchange" /> CHANNELS
-        </span>{" "}
-        ({channels.length}) <Icon name="add" />
-      </Menu.Item>
-    </Menu.Menu>
+    <>
+      <Menu.Menu
+        style={{
+          paddingBottom: "2em",
+        }}
+      >
+        <Menu.Item>
+          <span>
+            <Icon name="exchange" /> CHANNELS
+          </span>{" "}
+          ({channels.length}) <Icon name="add" onClick={() => setModal(true)} />
+        </Menu.Item>
+      </Menu.Menu>
+
+      <Modal basic open={modal} onClose={() => setModal(false)}>
+        <Modal.Header>Add a Channel</Modal.Header>
+        <Modal.Content>
+          <Form>
+            <Form.Field>
+              <Input
+                fluid
+                label="Name of Channel"
+                name="channelName"
+                onChange={handleChange}
+              />
+            </Form.Field>
+            <Form.Field>
+              <Input
+                fluid
+                label="About the Channel"
+                name="channelDetails"
+                onChange={handleChange}
+              />
+            </Form.Field>
+          </Form>
+        </Modal.Content>
+        <Modal.Actions>
+          <Button color="green" inverted>
+            <Icon name="checkmark" /> Add
+          </Button>
+          <Button color="red" inverted onClick={() => setModal(false)}>
+            <Icon name="remove" /> Cancel
+          </Button>
+        </Modal.Actions>
+      </Modal>
+    </>
   );
 };
 
