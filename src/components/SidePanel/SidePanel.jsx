6134bd5d18819a23647b90b7afed27dfb8069346
Mher Hovhannisyan <mh3r99@gmail.com>
2022-12-22 22:59:44 +0400

    channels component

--- src/components/SidePanel/SidePanel.jsx
+++ src/components/SidePanel/SidePanel.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Menu } from "semantic-ui-react";
+import Channels from "./Channels";
 import UserPanel from "./UserPanel";
 
 const SidePanel = ({ currentUser }) => {
@@ -12,6 +13,7 @@ const SidePanel = ({ currentUser }) => {
       style={{ background: "#4c3c4c", fontSize: "1.2rem" }}
     >
       <UserPanel currentUser={currentUser} />
+      <Channels />
     </Menu>
   );
 };
