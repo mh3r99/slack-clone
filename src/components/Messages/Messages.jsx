1f6abaa37e1dc6067e61ee19856a74967c0692fe
Mher Hovhannisyan <mh3r99@gmail.com>
2022-12-24 21:12:09 +0400

    ColorPanel & Message Components

--- src/components/Messages/Messages.jsx
+++ src/components/Messages/Messages.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { Segment, Comment } from "semantic-ui-react";
+import MessageForm from "./MessageForm";
+import MessagesHeader from "./MessagesHeader";
 
 const Messages = () => {
-  return <div>Messages</div>;
+  return (
+    <>
+      <MessagesHeader />
+      <Segment>
+        <Comment.Group className="messages"></Comment.Group>
+      </Segment>
+
+      <MessageForm />
+    </>
+  );
 };
 
 export default Messages;
